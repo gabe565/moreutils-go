df34f55f9da8bd5735a466c2829f655b379ef1f9
Gabe Cook <gabe565@gmail.com>
2024-09-17 17:58:42 -0500

    chore(ts): Remove unnecessary func

--- cmd/ts/ts.go
+++ cmd/ts/ts.go
@@ -54,11 +54,7 @@ func run(cmd *cobra.Command, args []string) error {
 
 	scanner := bufio.NewScanner(cmd.InOrStdin())
 	for scanner.Scan() {
-		_, _ = fmt.Fprintln(cmd.OutOrStdout(), timestamp(format), scanner.Text())
+		_, _ = fmt.Fprintln(cmd.OutOrStdout(), time.Now().Format(format), scanner.Text())
 	}
 	return scanner.Err()
-}
-
-func timestamp(format string) string {
-	return time.Now().Format(format)
 }
