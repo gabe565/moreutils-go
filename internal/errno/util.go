636f6d1de13d84e64043f841102615263689aeb0
Gabe Cook <gabe565@gmail.com>
2024-09-25 00:16:50 -0500

    fix(errno): Fix some mips and zos errors being missed

--- internal/errno/util.go
+++ internal/errno/util.go
@@ -6,18 +6,11 @@ import "iter"
 
 func Iter() iter.Seq[*Errno] {
 	return func(yield func(*Errno) bool) {
-		for num := range 256 {
+		for num := range 2048 {
 			if errno := New(num); errno.Valid() {
 				if !yield(errno) {
 					return
 				}
-			}
-		}
-
-		// MIPS has an error code > 256
-		if errno := New(1133); errno.Valid() {
-			if !yield(errno) {
-				return
 			}
 		}
 	}
